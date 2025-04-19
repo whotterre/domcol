b96333e4b4a039fd712f774f16992e53f72005e9
hashkitti001 <iJeddy5.0@gmail.com>
2025-04-19 11:02:15 +0100

    add cmd line flag logic

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"regexp"
+)
+
 func main(){
+	fmt.Println("DomCol\n. DomCol is a CLI tool that helps you") 
+	fmt.Println("find the dominant colors in an image file.")			
+	// Get flags
+	imgPath := flag.String("imgpath", "", "Path to the image file")
+	rgba := flag.Bool("rgba", false, "Returns the dominant colors in RGB form")
+	hex := flag.Bool("hex", false, "Returns the dominant colors in hex form")
+
+	
 	
 }
